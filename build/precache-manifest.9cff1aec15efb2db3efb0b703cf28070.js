7925aeb12be776ea92307fd7f16bad1d5c1f16cf
ivansolobear <ivansolobear@gmail.com>
2019-07-16 19:00:00 +0300

    build

---
+++ build/precache-manifest.9cff1aec15efb2db3efb0b703cf28070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d70dc83e4efdb20f268f",
+    "url": "/static/js/main.080aaadf.chunk.js"
+  },
+  {
+    "revision": "cf835da0f484a17c8d14",
+    "url": "/static/js/2.94b09b98.chunk.js"
+  },
+  {
+    "revision": "d70dc83e4efdb20f268f",
+    "url": "/static/css/main.61a9426f.chunk.css"
+  },
+  {
+    "revision": "cf835da0f484a17c8d14",
+    "url": "/static/css/2.ba5cd532.chunk.css"
+  },
+  {
+    "revision": "33079fc463aba3112cd77c652d56f91d",
+    "url": "/index.html"
+  }
+];
